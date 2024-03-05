cc3f2897577d2fcca2c64b8063f7d616fb8de673
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-05 09:31:27 +0530

    bugs

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 // import bannerImg from "../assets/trybanner.png";
 // import profile from "/profile.png";
 import skills from "../assets/skills.png";
+import { GlobalContext } from "../context/context";
 // import test from "../assets/testme.png";
 
 function Banner() {
+  const { darkMode } = useContext(GlobalContext);
   return (
     <div className="flex h-[100vh] max-w-6xl justify-center items-center">
       <div className=" w-[50%] items-center justify-center mobile:hidden lg:flex ml-20">
@@ -12,7 +14,7 @@ function Banner() {
       </div>
       <div className="flex w-full justify-center items-center p-10 ">
         <div className="flex flex-col gap-1">
-          <span className="mobile:text-sm md:text-xl lmobile:text-lg">
+          <span className={` mobile:text-sm md:text-xl lmobile:text-lg`}>
             Hello!{" "}
           </span>
           <h1 className="md:text-5xl mobile:text-xl lmobile:text-3xl">
@@ -22,7 +24,11 @@ function Banner() {
               Syed Farazuddin
             </span>
           </h1>
-          <p className="text-mywhite md:text-xl mobile:text-[16px] mt-1 md:font-bold ">
+          <p
+            className={`${
+              darkMode ? "text-mywhite" : "text-mdark"
+            }  md:text-xl mobile:text-[16px] mt-1 md:font-bold `}
+          >
             <span>MERN DEVELOPER</span>{" "}
             <span className="mobile:hidden md:inline-block">
               / JAVA DEVELOPER{" "}
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -19,7 +19,9 @@ function Homepage() {
       <Nav />
       <div className="mobile:px-0 mobile:py-10 lg:px-10 lg:py-10 flex flex-col items-center justify-center relative">
         <div
-          className=" right-5 text-white top-20 h-10 w-10 bg-customblue rounded-full flex justify-center items-center fixed z-30"
+          className={`${
+            darkMode ? " bg-customblue text-white " : "text-black bg-slight"
+          } mobile:w-8 mobile:right-3 mobile:h-8 sm:right-5  top-20 sm:h-10 sm:w-10 rounded-full flex justify-center items-center fixed z-30`}
           onClick={() => {
             setDarkMode((prev) => !prev);
           }}
